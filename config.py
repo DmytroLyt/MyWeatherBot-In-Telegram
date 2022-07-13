58a22c224ce571556f00a4a1d242a78178c19bb1
Dmytro Lytvynenko <105833335+DmytroLyt@users.noreply.github.com>
2022-07-13 06:22:48 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 open_weather_token = "0acbe52d9f25ce0b129bcd97e946b693"
-tg_bot_token = "Here should be token of your created bot in Bot_Father in Telegram
+tg_bot_token = "Here should be token of your created bot in Bot_Father in Telegram"
